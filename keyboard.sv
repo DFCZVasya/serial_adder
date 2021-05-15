c1efc91ddf5fdff039afa438a4458475d877196a
Vasya Lopatkin <48513210+DFCZVasya@users.noreply.github.com>
2021-05-15 10:53:10 +0700

    Update keyboard.sv

--- keyboard.sv
+++ keyboard.sv
@@ -23,24 +23,24 @@ reg [2:0] j = 0;
 
 always @ (posedge clk) begin
     if (column[0]) begin
-        out = i * 4 + 0;
+        out = i + 0;
         flag = 1'b1;
     end else
-            if (column[1]) begin
-                out = i * 4 + 1;
-                flag = 1'b1;
-            end else
-                    if (column[2]) begin
-                        out = i * 4 + 2;
-                        flag = 1'b1;
-                    end else
-                            if (column[3]) begin
-                                out = i * 4 + 3;
-                                flag = 1'b1;
-                            end else begin
-                                out = 4'b0000;
-                                flag = 1'b0;
-                            end
+    if (column[1]) begin
+        out = i + 4;
+        flag = 1'b1;
+    end else
+    if (column[2]) begin
+        out = i + 8;
+        flag = 1'b1;
+    end else
+    if (column[3]) begin
+        out = i + 12;
+        flag = 1'b1;
+    end else begin
+        out = 4'b0000;
+        flag = 1'b0;
+    end
 end
 
 always @(posedge clk) begin
@@ -48,38 +48,6 @@ always @(posedge clk) begin
     i = i + 1;
     line[i] = 1'b1;
 end
-
-/*always @ (posedge clk ) begin
-line[i] = 0;
-if (i < 3'b100) begin
-  line[i] = 1;
-     if (j < 3'b100) begin
-        if (column[j] && line[i]) begin
-            flag = 1;
-            out = ((i + 1) + (j + 1) * 4);
-            //line = 4'b0000;
-            //i = 3'b000;
-            //j = 3'b000;
-        end
-        else begin
-            flag = 0;
-            out = 0;
-        end
-        j = j + 1;
-     end
-     else begin
-        i = i + 1;
-        j = 3'b000;
-     end
-end
-else begin
-i = 3'b000;
-line = 4'b0000;
-out = 5'b00000;
-end
-end*/
-
-
 
 
 endmodule
